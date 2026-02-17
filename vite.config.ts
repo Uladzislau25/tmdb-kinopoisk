b1cc12494d67f4cdd9732eae6155729602eec097
Uladzislau <u.shylko25@gmail.com>
2026-02-17 14:11:07 +0300

    feat: fixed bugs

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,11 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/",
   resolve: {
     alias: {
       "@/": `${path.resolve(__dirname, "src")}/`,
     },
   },
 })
+
