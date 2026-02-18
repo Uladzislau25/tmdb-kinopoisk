3f662b2b999f43f2ac7dffb0de1abf9537274efb
Uladzislau <u.shylko25@gmail.com>
2026-02-18 23:11:50 +0300

    feat: add sort_by title

--- src/features/movies/FilteredMovies/ui/FilteredMovies.tsx
+++ src/features/movies/FilteredMovies/ui/FilteredMovies.tsx
@@ -1,10 +1,9 @@
-import { useEffect, useMemo, useState } from "react"
+import { useEffect,  useState } from "react"
 import { useDebounce } from "@/common/hooks/useDebounce.ts"
 import { Pagination } from "@/common/components/Pagination/Pagination.tsx"
 import { FiltersPanel } from "@/features/movies/FilteredMovies/ui/FiltredPanel/FiltredPanel.tsx"
 import s from "./FiltredMovies.module.css"
 import { useGetMoviesDiscoverQuery } from "@/features/movies/FilteredMovies/api/filtredApi.ts"
-import type { Movie } from "@/app/schemas/moviesApi.schema.ts"
 import { MovieSkeleton } from "@/common/components/MovieSkeleton/MovieSkeleton.tsx"
 import { ResultSort } from "@/features/movies/FilteredMovies/ui/ResultSort/ResultSort.tsx"
 
@@ -19,26 +18,14 @@ export const FilteredMovies = () => {
   }, [sortBy, genres, rating])
 
   const debouncedRating = useDebounce(rating, 200)
-  const isTitleSort = sortBy.startsWith("title")
 
   const { data, isFetching } = useGetMoviesDiscoverQuery({
-    sort_by: isTitleSort ? "popularity.desc" : sortBy,
+    sort_by: sortBy,
     "vote_average.gte": debouncedRating[0],
     "vote_average.lte": debouncedRating[1],
     with_genres: genres.join(","),
     page,
   })
-
-  const sortedMovies = useMemo<Movie[]>(() => {
-    if (!data?.results) return []
-
-    if (isTitleSort) {
-      return [...data.results].sort((a, b) =>
-        sortBy === "title.asc" ? a.title.localeCompare(b.title) : b.title.localeCompare(a.title),
-      )
-    }
-    return data.results
-  }, [data?.results, sortBy, isTitleSort])
 
   const resetFilters = () => {
     setSortBy("popularity.desc")
@@ -63,7 +50,7 @@ export const FilteredMovies = () => {
         <MovieSkeleton width={150} height={250} columns={5} rows={4} />
       ) : (
         <section className={s.box}>
-          <ResultSort movies={sortedMovies} />
+          <ResultSort movies={data?.results ?? []} />
 
           {(data?.total_pages ?? 0) > 1 && <Pagination page={page} totalPages={data?.total_pages} onChange={setPage} />}
         </section>
